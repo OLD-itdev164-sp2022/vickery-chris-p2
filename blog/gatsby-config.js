f57aa89feff013dbe6f529e67c2aa923ffab444a
Chris Vickery <vickeryc@gmail.com>
2022-04-29 14:24:41 -0500

    Setup complete

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,8 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Warhammer 40k :Necrons`,
+    description: `A blog for the silver tide`,
+    author: `Chris Vickery`,
+    contact: {
+      name: `chris vickery`,
+      company: `vicinc`,
+      address: `redacted`,
+    },
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
